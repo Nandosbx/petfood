ec4ac7e53470e3fde1542976f779a7b51b4f9700
Fernando <nandosbx@users.noreply.github.com>
2021-08-01 23:36:29 -0300

    Main Route

---
+++ api/src/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const URI = 'mongodb://localhost:8000/petfood'
+
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+mongoose.set('debug', true)
+
+mongoose
+    .connect(URI)
+    .then(() => {
+        console.log('DB is running')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
